07d8ddc1f405269f072249dfd8028501a8b00d65
Bahadir Eker <66503801+ChaoticPower@users.noreply.github.com>
2023-07-21 21:00:45 +0000

    Testing Changes for Image

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var imgArray = ["images/meC.png","images/meG.png","images/meP.png","images/meR.png","images/meK.png","images/meA.png"];
+  // var imgArray = ["images/meC.png","images/meG.png","images/meP.png","images/meR.png","images/meK.png","images/meA.png"];
   var btnIds = ["btnC","btnG","btnP","btnR","btnK","btnA"];
   var colorNames = ["chartreuse","khaki","indianred","cyan","sienna","lightskyblue"]
-  var container = document.getElementById("imageContainer");
+  // var container = document.getElementById("imageContainer");
   const btnLeft = btnIds.slice(0, 3); // Used in Comment "Font Gradient Transition"
 
-  for (var i = 0; i < imgArray.length; i++) {
+  for (var i = 0; i < btnIds.length; i++) {
     (function(i){
 
-      var img = document.createElement("img");
-      img.style.position = "fixed";
+      // var img = document.createElement("img");
+      // img.style.position = "fixed";
       var color = colorNames[i];
       var btn = document.getElementById(btnIds[i]);
-      img.src = imgArray[i];
-      container.appendChild(img);
+      // img.src = imgArray[i];
+      // container.appendChild(img);
 
       btn.addEventListener("mouseover", function(){
 
-        var imgElements = Array.from(document.querySelectorAll("#imageContainer img"));
-        imgElements.forEach(function(element) { // Hide all Pictures
-          element.style.visibility = "hidden";
-        })
+        // var imgElements = Array.from(document.querySelectorAll("#imageContainer img"));
+        // imgElements.forEach(function(element) { // Hide all Pictures
+        //   element.style.visibility = "hidden";
+        // })
 
-        img.style.visibility = "visible"; // Show One Picture
+        // img.style.visibility = "visible"; // Show One Picture
         
         // Font Gradient Transition
         btn.style.backgroundPosition = btnLeft.includes(btn.id) ? "100% 0%" : "0% 0%";
@@ -37,32 +37,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
     })(i);
   }
-
-    /* Button Movement Animation */
-    $(document).ready(function(){
-      animateDiv('btn');
-  });
-
-  function makeNewPosition(){
-    
-    // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() - 50;
-    var w = $(window).width() - 50;
-    
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-    
-    return [nh,nw];    
-    
-  }
-
-  function animateDiv(myclass){
-      var newq = makeNewPosition();
-      $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
-        animateDiv(myclass);        
-      });
-      
-  };
-
 });
 
