2c4c0bb38852e5721096c7fedbe74e55a7292c7f
Bahadir Eker <66503801+ChaoticPower@users.noreply.github.com>
2023-07-24 22:21:03 +0000

    Small Changed to Contact

--- js/contact.js
+++ js/contact.js
@@ -2,9 +2,10 @@ function sendEmail(){
     Email.send({
         SecureToken : "a738e7a3-b265-4ecf-8487-a88056c2b603",
         To : document.getElementById("email").value,
-        From : 'Bahadir47Eker@Gmail.com',
+        From : 'Bahadir47Eker@gmail.com',
         Subject : document.getElementById("subject").value,
-        Body : "Email : " + document.getElementById("email").value
+        Body : "Name : " + document.getElementById("fname").value
+            + "<br> Email : " + document.getElementById("email").value
             + "<br> Betreff : " + document.getElementById("reference").value
     }).then(
     message => alert(message)
