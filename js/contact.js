400d6549b070f21728f2c19d36434b62c85fb6d5
Bahadir Eker <66503801+ChaoticPower@users.noreply.github.com>
2023-07-24 21:31:27 +0000

    New Contact html/css

--- js/contact.js
+++ js/contact.js
@@ -3,9 +3,9 @@ function sendEmail(){
         SecureToken : "a738e7a3-b265-4ecf-8487-a88056c2b603",
         To : document.getElementById("email").value,
         From : 'Bahadir47Eker@Gmail.com',
-        Subject : "Email From Portfolio Website",
+        Subject : document.getElementById("subject").value,
         Body : "Email : " + document.getElementById("email").value
-            + "<br> Betreff : " + document.getElementById("subject").value
+            + "<br> Betreff : " + document.getElementById("reference").value
     }).then(
     message => alert(message)
     );
