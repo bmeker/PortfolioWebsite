86ed9ad0b5db60b7392b659fedc9d754893b3ef5
Bahadir Eker <66503801+ChaoticPower@users.noreply.github.com>
2023-07-25 19:09:52 +0000

    Contact Form Fixed

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,8 @@
 function sendEmail(){
     Email.send({
-        SecureToken : "a738e7a3-b265-4ecf-8487-a88056c2b603",
-        To : document.getElementById("email").value,
-        From : 'Bahadir47Eker@gmail.com',
+        SecureToken : "cac3bb45-ec2b-44d5-afd8-63cd83d519c4",
+        To : 'Bahadir47Eker@gmail.com',
+        From : 'portfolio.contact.me.mail@gmail.com',
         Subject : document.getElementById("subject").value,
         Body : "Name : " + document.getElementById("fname").value
             + "<br> Email : " + document.getElementById("email").value
