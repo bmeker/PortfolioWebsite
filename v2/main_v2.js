a76d0c1b838436d8db1f71645b79dec5b2405dc4
Bahadir Eker <66503801+ChaoticPower@users.noreply.github.com>
2023-07-28 21:34:18 +0000

    Changes in v2 design

--- v2/main_v2.js
+++ v2/main_v2.js
@@ -1,40 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    var navIds = ["contact","github","project","resume","knowledge","about_me"];
-    // var colorDegree = ["290deg","230deg","180deg","140deg","80deg","0deg"];
+    var btnIds = ["contact","github","project","resume","knowledge","about_me"];
+    var colorDegree = ["290deg","230deg","180deg","140deg","80deg","0deg"];
   
     for (var i = 0; i < btnIds.length; i++) {
       (function(i){
   
-        // var colorDeg = colorDegree[i];
-        var nav = document.getElementById(navIds[i]);
+        var colorDeg = colorDegree[i];
+        var btn = document.getElementById(btnIds[i]);
   
-        // navIds.forEach(element => { // Button Position
-        //   var pos = JSON.parse(nav.dataset.pos);
-        //   btn.style.top = pos[0] + "vh";
-        //   btn.style.left = pos[1] + "vh";
-        // });
-  
-        nav.addEventListener("mouseover", function(){ // Mouse In Hover
-        //   document.documentElement.style.setProperty("--hue-deg", colorDeg);
-          nav.style.backgroundPosition ? "100% 0%" : "0% 0%";  // Font Gradient Transition
+        btn.addEventListener("mouseover", function(){ // Mouse In Hover
+          document.documentElement.style.setProperty("--hue-deg", colorDeg);
+          btn.style.backgroundPosition = btn.style.backgroundPosition ? "0% 0%" : "100% 0%";
+          btn.style.transform = "translateX(40px)";
+          btn.style.transition = '0.5s';
         });
   
-        nav.addEventListener("mouseout", function(){ // Mouse Out Hover
-          nav.style.backgroundPosition ? "0% 0%" : "100% 0%"; // Font Gradient Transition
+        btn.addEventListener("mouseout", function(){ // Mouse Out Hover
+          btn.style.backgroundPosition = btn.style.backgroundPosition ? "100% 0%" : "0% 0%";
+          btn.style.transform = "translateX(0px)";
+          btn.style.transition = '0.5s';
         });
   
       })(i);
     }
   });
   
+  // function openContactAnim() { // Opening Contact Form
+  //   document.getElementById('center').style.animation = 'enableBackdropBlur 1s forwards';
+  //   document.getElementById('contact_container').style.animation = 'openContactForm 1s forwards'; // 2s = duration
+  // }
+  // function closeContactAnim(){ // Closing Contact Form
+  //   document.getElementById('center').style.animation = 'disableBackdropBlur 1s forwards';
+  //   document.getElementById('contact_container').style.animation = 'closeContactForm 1s forwards'; // 2s = duration
+  // }
   
-  function openContactAnim() { // Opening Contact Form
-    document.getElementById('center').style.animation = 'enableBackdropBlur 1s forwards';
-    document.getElementById('contact_container').style.animation = 'openContactForm 1s forwards'; // 2s = duration
-  }
-  function closeContactAnim(){ // Closing Contact Form
-    document.getElementById('center').style.animation = 'disableBackdropBlur 1s forwards';
-    document.getElementById('contact_container').style.animation = 'closeContactForm 1s forwards'; // 2s = duration
-  }
   
